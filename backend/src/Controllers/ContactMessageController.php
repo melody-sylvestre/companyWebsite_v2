88be7e3891520fa5fe6d0052fbd381df2a7143ee
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-06-26 17:37:29 +0100

    a 1st version of the contact-us route

--- backend/src/Controllers/ContactMessageController.php
+++ backend/src/Controllers/ContactMessageController.php
@@ -6,8 +6,11 @@ use Slim\Http\Response as Response;
 use Slim\Http\ServerRequest as Request;
 
 use App\Models\ContactMessageModel;
+use App\Sanitisers\ContactMessageSanitiser;
+use App\Validators\ContactMessageValidator;
 
-class ContactMessageController{
+class ContactMessageController
+{
 
     private ContactMessageModel $ContactMessageModel;
 
@@ -16,22 +19,41 @@ class ContactMessageController{
         $this->ContactMessageModel = $contactMessageModel;
     }
 
-    public function __invoke (Request $request, Response $response, $args) 
+    public function __invoke(Request $request, Response $response, $args)
     {
         $requestBody = $request->getParsedBody();
-        //sanitise & validate
-        // sanitise all fields
-        // call all the validors
-        // if a validator trigger an exception Exception, return 400 and specific error message
-        $validContactMessage = $requestBody;
+        $sanitisedContactMessageForm = [];
 
+        foreach ($requestBody as $fieldName => $fieldValue){
+            $sanitisedContactMessageForm[$fieldName] = ContactMessageSanitiser::stringSanitiser($fieldValue);
+        }
+        try {
+            $fullNameIsValid = ContactMessageValidator::validateFullName($sanitisedContactMessageForm);
+            $EmailAddresIsValid = ContactMessageValidator::validateEmailAddress($sanitisedContactMessageForm);
+            $PhoneNumber1IsValid = ContactMessageValidator::validatePhoneNumber1($sanitisedContactMessageForm);
+            $PhoneNumber2IsValid = ContactMessageValidator::validateAdditionalPhoneNumber($sanitisedContactMessageForm["PhoneNumber2"]);
+            $PhoneNumber3IsValid = ContactMessageValidator::validateAdditionalPhoneNumber($sanitisedContactMessageForm["PhoneNumber3"]);
+            $MessageTextIsValid = ContactMessageValidator::validateMessageText($sanitisedContactMessageForm);
+            $bIncludeAddressDetailsIsValid = ContactMessageValidator::validatebIncludeAddressDetails($sanitisedContactMessageForm);
+            $AddressLine1IsValid = ContactMessageValidator::validateAddressLine1($sanitisedContactMessageForm);
+            $AddressLine2IsValid = ContactMessageValidator::validateAddressLine2($sanitisedContactMessageForm);
+            $CityTownIsValid = ContactMessageValidator::validateCityTown($sanitisedContactMessageForm);
+            $StateCountyIsValid = ContactMessageValidator::validateStateCounty($sanitisedContactMessageForm);
+            $PostcodeIsValid = ContactMessageValidator::validatePostcode($sanitisedContactMessageForm);
+            $CountryIsValid = ContactMessageValidator::validateCountry($sanitisedContactMessageForm);
+        } catch (\Exception $contactMessageException) {
+            error_log($contactMessageException -> getMessage() . "\n", 3, 'logs/serverlog.log'); 
+            return $response->withStatus(400);
+        }
+        
         //post request and return code
-        $this->ContactMessageModel->postContactMessage($validContactMessage);
-        
-        //generate response with the right format
-        return $response->withStatus(200)->withJson($validContactMessage);
+        try {
+            $this->ContactMessageModel->postContactMessage($sanitisedContactMessageForm);
+            return $response->withStatus(200);
 
-        // error handling
-        
+        } catch (\Exception $exception) {
+            return $response->withStatus(500);
+        }
+               
     }
 }
--- backend/src/Validators/ContactMessageValidator.php
+++ backend/src/Validators/ContactMessageValidator.php
@@ -2,83 +2,206 @@
 
 namespace App\Validators;
 
-use phpDocumentor\Reflection\Types\Boolean;
-
 class ContactMessageValidator 
 {
     const MAX_STRING_LENGTH = 50;
     const MAX_MESSAGE_LENGTH = 2000;
 
-    // validators either return true or throw invalidFormException
-    public static function validateFullName (array $contactMessage) : boolean
+    public static function validateFullName (array $contactMessage) : bool
     {
-        // check if FullName is not empty and < max_string_length
+        if($contactMessage["FullName"]!=="" && strlen($contactMessage["FullName"])<= self::MAX_STRING_LENGTH) {
+           return true; 
+        } else {
+            throw new \Exception("Invalid FullName"); 
+        }
     }
 
-    public static function validateEmailAddress (array $contactMessage) : boolean
+    public static function validateEmailAddress (array $contactMessage) : bool
     {
-        // check if email address is valid
+        if(filter_var($contactMessage["EmailAddress"], FILTER_VALIDATE_EMAIL)){
+            return true;
+        } else {
+            throw new \Exception("Invalid EmailAddress"); 
+        }       
     }
 
-    public static function validatePhoneNumber1 (array $contactMessage) : boolean
+    public static function validatePhoneNumber1 (array $contactMessage) : bool
     {
-        // if phoneNumber is not empty -  check if valid (only a set numbers of digits  including prefix - 10max?)
+        $phoneNumberRegex = "/^\+(?:[0-9]\x20?){6,14}[0-9]$/";
+        if(preg_match($phoneNumberRegex, $contactMessage["PhoneNumber1"])){
+            return true;
+        } else {
+            throw new \Exception("Invalid PhoneNumber1"); 
+        }       
     }
     
-    public static function validatePhoneNumber2 (array $contactMessage) : boolean
+    public static function validateAdditionalPhoneNumber (string $phoneNumber) : bool
     {
-        // if phoneNumber is not empty -  check if valid (only a set numbers of digits  including prefix - 10max?)
+        if($phoneNumber === ""){
+            return true;
+        } else {
+            $phoneNumberRegex = "/^\+(?:[0-9]\x20?){6,14}[0-9]$/";
+            if(preg_match($phoneNumberRegex,$phoneNumber)){
+                return true;
+            } else {
+                throw new \Exception("Invalid additional phone number"); 
+            }     
+        }
     }
 
-    public static function validatePhoneNumber3 (array $contactMessage) : boolean
+    public static function validateMessageText (array $contactMessage) : bool
     {
-        // if phoneNumber is not empty -  check if valid (only a set numbers of digits  including prefix - 10max?)
+
+        if( strlen($contactMessage["MessageText"]) > 0 && strlen($contactMessage["MessageText"]) <= self::MAX_MESSAGE_LENGTH) {
+            return true;
+        } else {
+            throw new \Exception("Invalid MessageText");   
+        } 
     }
 
-    public static function validateMessageText (array $contactMessage) : boolean
+    public static function validatebIncludeAddressDetails (array $contactMessage) : bool
     {
-        // should not be empty and should be less than set Length
+        if ($contactMessage["bIncludeAddressDetails"] == "0" || $contactMessage["bIncludeAddressDetails"] == "1"){
+            return true;
+        } else {
+            throw new \Exception("Invalid bIncludeAddressDetails");
+        }
     }
 
-    public static function validatebIncludeAddressDetails (array $contactMessage) : boolean
+    public static function validateAddressLine1(array $contactMessage): bool
     {
-        //should be 0 or 1
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["AddressLine1"])=== 0 ) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid bIncludeAddressDetails or AddressLine1");   
+            
+            }         
+        } else {
+            #checking that there are no special characters and no punctuations
+            if(strlen($contactMessage["AddressLine1"]) > 0 && strlen($contactMessage["AddressLine1"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-Z\d\s]/", $contactMessage["AddressLine1"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid AddressLine1");   
+            }      
+        }                
+    }
+    
+
+    public static function validateAddressLine2 (array $contactMessage): bool
+    {
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["AddressLine2"]) !== 0 ) {
+                throw new \Exception("Invalid bIncludeAddressDetails or AddressLine2");    
+            
+            } else {
+                return true;
+            
+            }  
+            
+        } else {
+            if (strlen($contactMessage["AddressLine2"]) === 0) {
+             return true; 
+            
+            } else if (strlen($contactMessage["AddressLine2"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-Z\d\s]/", $contactMessage["AddressLine1"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid AddressLine2");
+            }
+                  
+        }         
     }
 
-    public static function validateAddressLine1(array $contactMessage): boolean 
+    public static function validateCityTown (array $contactMessage): bool
     {
-        // check that bIncludeAddressDetails is true
-        // should not be empty
-        // check that AddressLine1 contains only alpha numeric characters
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["CityTown"])=== 0 ) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid bIncludeAddressDetails or CityTown");   
+            
+            }         
+        } else {
+            // checking that City/Town field does not contain digits or special characters except -
+            if(strlen($contactMessage["CityTown"]) > 0 && strlen($contactMessage["CityTown"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-z\s\-]/", $contactMessage["CityTown"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid CityTown");   
+            }      
+        }
     }
 
-    public static function validateAddressLine2 (array $contactMessage): boolean 
+    public static function validateStateCounty (array $contactMessage): bool
     {
-        // check that bIncludeAddressDetails is true
-        // check that if non empty AddressLine2 contains only alpha numeric characters
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["StateCounty"])=== 0 ) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid bIncludeAddressDetails or StateCounty");   
+            
+            }         
+        } else {
+            // checking that State/County field does not contain any digit or special character except -
+            if(strlen($contactMessage["StateCounty"]) > 0 && strlen($contactMessage["StateCounty"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-z\s\-]/", $contactMessage["StateCounty"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid StateCounty");   
+            }      
+        }
     }
 
-    public static function validateCityTown (array $contactMessage): boolean 
+    public static function validatePostcode (array $contactMessage): bool 
     {
-        // check that bIncludeAddressDetails is true
-        //should be non empty, only letters and not longert than set Length
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["Postcode"])=== 0 ) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid bIncludeAddressDetails or Postcode");   
+            
+            }         
+        } else {
+            // checking that State/County field does not contain any special characters except -
+            if(strlen($contactMessage["Postcode"]) > 0 && strlen($contactMessage["Postcode"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-Z\d\s\-]/", $contactMessage["Postcode"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid Postcode");   
+            }      
+        }   
     }
 
-    public static function validateStateCounty (array $contactMessage): boolean 
+    public static function validateCountry (array $contactMessage): bool
     {
-        // check that bIncludeAddressDetails is true
-        //if non empty, should be only letters and not longerthan set Length
-    }
-
-    public static function validatePostcode (array $contactMessage): boolean 
-    {
-        // check that bIncludeAddressDetails is true
-        //should not be empty, should be only letters and digits and not longer than set Length
-    }
-
-    public static function validateCountry (array $contactMessage): boolean 
-    {
-        // check that bIncludeAddressDetails is true
-        // should not be empty, should be only letters and not longer than set Length
+        if($contactMessage["bIncludeAddressDetails"] === "0") {
+        
+            if( strlen($contactMessage["Country"])=== 0 ) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid bIncludeAddressDetails or Country");   
+            
+            }         
+        } else {
+            // checking that Country field does not contain digits or special characters except -
+            if(strlen($contactMessage["Country"]) > 0 && strlen($contactMessage["Country"]) <= self::MAX_STRING_LENGTH && !preg_match("/[^a-zA-z\s\-]/", $contactMessage["Country"])) {
+                return true;
+            
+            } else {
+                throw new \Exception("Invalid Country");   
+            }      
+        }
     }
 }
