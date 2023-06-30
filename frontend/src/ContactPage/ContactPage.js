3b317991c8643feca5e7a65408e4cc6ac839bed4
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-06-30 14:41:11 +0100

    improved responsiveness of Contact page

--- frontend/src/ContactPage/ContactPage.js
+++ frontend/src/ContactPage/ContactPage.js
@@ -1,10 +1,27 @@
-import ContactMain from "../ContactMain/ContactMain"
+import { useState } from "react"
+import ContactForm from "../ContactForm/ContactForm"
+import FormSubmittedBox from "../FormSubmittedBox/FormSubmittedBox"
+import "./ContactPage.scss"
 
-const ContactPage = () => {
+const ContactMain = () => {
+
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
     return (
-        <div>
-            <ContactMain/>    
-        </div>
+        <main className="contact_main">
+            <h1>Contact Us</h1>
+
+            <div className="contact_image_and_textbox">
+
+                <div className="contact_text_container">
+                    <p>Et aperiam quod quo voluptatem magnam qui beatae voluptatem id doloremque quod. Ea sunt nesciunt et reiciendis vitae ea alias provident 33 ratione iusto. </p>
+                    {formSubmitted ? <FormSubmittedBox /> : <ContactForm setFormSubmitted={setFormSubmitted} />}
+                </div>
+                <img src="images/logo.jpg" alt="Company logo" className="side_image" />
+            </div>
+        </main>
+
     )
 }
-export default ContactPage
+
+export default ContactMain
--- frontend/src/Footer/Footer.js
+++ frontend/src/Footer/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer>
             <p className="main_credits">Website by <a className="main_credits" href="/https://melody-sylvestre.github.io/">Melody Sylvestre</a></p>
-            <p className="additional_credits">Mountain photo by <a className="additional_credits" href="https://unsplash.com/@the_bracketeer">Hendrik Cornelissen</a> and lake photo <a className="additional_credits" href="https://unsplash.com/@luckybeanz">Mark Harpur</a></p>
+            <p className="additional_credits">Additional photos by <a className="additional_credits" href="https://unsplash.com/@the_bracketeer">Hendrik Cornelissen</a> and <a className="additional_credits" href="https://unsplash.com/@luckybeanz">Mark Harpur</a></p>
         </footer>
     )
 }
